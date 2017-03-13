163a3289f2b6d7b4129e86a2181e9f1234aeab34
boogiebro <jkov5150@gmail.com>
2017-03-13 15:53:19 -0400

    Add calc.rb

--- calc.rb
+++ calc.rb
@@ -1,4 +1,35 @@
+=begin
 puts 'Hello there, and what\'s your name?'
 name = gets.chomp
 puts 'Your name is ' + name + '? What a lovely name!'
 puts 'Pleased to meet you, ' + name + '. :)'
+=end 
+	
+=begin
+puts 'Hello there, what\'s your first name?'
+name_first = gets.chomp
+puts 'And what is your middle name?' 
+name_middle = gets.chomp
+puts '... Last name?'
+name_last = gets.chomp
+puts 'Good day! ' + name_first + ' ' + name_middle + ' ' + name_last + ' !'
+=end
+
+
+puts 'Hello there, what\'s your favourite number?'
+numFav = gets.chomp
+numBet = numFav.to_i + 1
+puts numFav + ' is a good number, but ' + numBet.to_s + ' is better!'
+
+
+
+
+
+
+
+
+
+
+
+
+
